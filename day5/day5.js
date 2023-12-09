484d7f3242c807391d2236c60ee5c83256fc44e1
ryanradder <ryan.radder@headfirst.nl>
2023-12-09 17:23:53 +0100

    -working (i think), out of memory

--- day5/day5.js
+++ day5/day5.js
@@ -1,7 +1,7 @@
 console.log('Hello day5');
 
 const fs = require('fs');
-const document = fs.readFileSync('day5-test.txt', 'UTF-8').split('\n').filter(line => line.trim() !== '');
+const document = fs.readFileSync('day5.txt', 'UTF-8').split('\n').filter(line => line.trim() !== '');
 const seeds = document[0].split(': ')[1].split(' ').map(Number);
 console.log('seeds:', seeds);
 
@@ -112,7 +112,7 @@ humidityToLocationMaps = humidityToLocation.map(line => createRanges(line));
             foundSeedToSoilNumbers.push(currentNumber);
         }
         currentNumber = getClosestValue(foundSeedToSoilNumbers, originalSeedToSoilNumber);
-        console.log('current-SeedToSoilNumber:', currentNumber);
+        // console.log('current-SeedToSoilNumber:', currentNumber);
 
         // Convert seed through soilToFertilizerMaps
         let originalSoilToFertilizerNumber = currentNumber;
@@ -129,7 +129,7 @@ humidityToLocationMaps = humidityToLocation.map(line => createRanges(line));
             foundSoilToFertilizerNumbers.push(currentNumber);
         }
         currentNumber = getClosestValue(foundSoilToFertilizerNumbers, originalSoilToFertilizerNumber);
-        console.log('current-SoilToFertilizerNumber:', currentNumber);
+        // console.log('current-SoilToFertilizerNumber:', currentNumber);
 
         // Convert seed through fertilizerToWaterMaps
         let originalFertilizerToWaterNumber = currentNumber;
@@ -146,7 +146,7 @@ humidityToLocationMaps = humidityToLocation.map(line => createRanges(line));
             foundFertilizerToWaterNumbers.push(currentNumber);
         }
         currentNumber = getClosestValue(foundFertilizerToWaterNumbers, originalFertilizerToWaterNumber);
-        console.log('current-FertilizerToWaterNumber:', currentNumber);
+        // console.log('current-FertilizerToWaterNumber:', currentNumber);
 
         // Convert seed through waterToLightMaps
         let originalWaterToLightNumber = currentNumber;
@@ -163,7 +163,7 @@ humidityToLocationMaps = humidityToLocation.map(line => createRanges(line));
             foundWaterToLightNumbers.push(currentNumber);
         }
         currentNumber = getClosestValue(foundWaterToLightNumbers, originalWaterToLightNumber);
-        console.log('current-WaterToLightNumber:', currentNumber);
+        // console.log('current-WaterToLightNumber:', currentNumber);
 
         // Convert seed through lightToTemperatureMaps
         let originalLightToTemperatureNumber = currentNumber;
@@ -180,7 +180,7 @@ humidityToLocationMaps = humidityToLocation.map(line => createRanges(line));
             foundLightToTemperatureNumbers.push(currentNumber);
         }
         currentNumber = getClosestValue(foundLightToTemperatureNumbers, originalLightToTemperatureNumber);
-        console.log('current-LightToTemperatureNumber:', currentNumber);
+        // console.log('current-LightToTemperatureNumber:', currentNumber);
 
         // Convert seed through temperatureToHumidityMaps
         let originalTemperatureToHumidityNumber = currentNumber;
@@ -197,7 +197,7 @@ humidityToLocationMaps = humidityToLocation.map(line => createRanges(line));
             foundTemperatureToHumidityNumbers.push(currentNumber);
         }
         currentNumber = getClosestValue(foundTemperatureToHumidityNumbers, originalTemperatureToHumidityNumber);
-        console.log('current-TemperatureToHumidityNumber:', currentNumber);
+        // console.log('current-TemperatureToHumidityNumber:', currentNumber);
 
         // Convert seed through humidityToLocationMaps
         let originalHumidityToLocationNumber = currentNumber;
@@ -215,7 +215,7 @@ humidityToLocationMaps = humidityToLocation.map(line => createRanges(line));
         }
         currentNumber = getClosestValue(foundHumidityToLocationNumbers, originalHumidityToLocationNumber);
 
-        console.log('current-HumidityToLocationNumber:', currentNumber);
+        // console.log('current-HumidityToLocationNumber:', currentNumber);
         return currentNumber;
     }
 
@@ -225,105 +225,11 @@ let locations = [];
 seeds.forEach((seed) => {
     const location = findCorrespondingLocation(seed);
     locations.push(location);
-    console.log('-------------------------');
-    console.log(`locations for seed ${seed} is: ${location}`);
-    console.log('-------------------------');
-    console.log('-------------------------');
-    const answer = Math.min(...locations);
-    console.log('answer:', answer);
+    // console.log('-------------------------');
+    // console.log(`locations for seed ${seed} is: ${location}`);
+    // console.log('-------------------------');
+    // console.log('-------------------------');
+
 });
-
-// console.log('seedToSoilMaps:', seedToSoilMaps);
-// console.log('fertilizerToWaterMaps:', fertilizerToWaterMaps);
-// console.log('waterToLightMaps:', waterToLightMaps);
-// console.log('lightToTemperatureMaps:', lightToTemperatureMaps);
-// console.log('temperatureToHumidityMaps:', temperatureToHumidityMaps);
-// console.log('humidityToLocationMaps:', humidityToLocationMaps);
-//
-
-// seeds.forEach((seed) => {
-//     console.log('-------------------------');
-//     console.log('seed:', seed);
-//     let seedToSoilLocations = [];
-//     seedToSoilMaps.forEach((seedToSoilMap, mapIndex) => {
-//         if(seedToSoilMap.sourceRanges.includes(seed)) {
-//             seedToSoilLocations.push(seedToSoilMap.destinationRanges[seedToSoilMap.sourceRanges.indexOf(seed)]);
-//         }
-//     });
-//     if(seedToSoilLocations.length === 0) {
-//         seedToSoilLocations.push(seed);
-//     }
-//     console.log('seedToSoilLocations:', seedToSoilLocations);
-//
-//     let soilToFertilizerLocations = [];
-//     soilToFertilizerMaps.forEach((map, index) => {
-//         if(map.sourceRanges.includes(seedToSoilLocations[index])) {
-//             soilToFertilizerLocations.push(map.destinationRanges[index]);
-//         }
-//     });
-//     if(soilToFertilizerLocations.length === 0) {
-//         soilToFertilizerLocations = [...seedToSoilLocations];
-//     }
-//     console.log('soilToFertilizerLocations:', soilToFertilizerLocations);
-//
-//     let fertilizerToWaterLocations = [];
-//     fertilizerToWaterMaps.forEach((map, index) => {
-//         if(map.sourceRanges.includes(soilToFertilizerLocations[index])) {
-//             fertilizerToWaterLocations.push(map.destinationRanges[map.sourceRanges[index]]);
-//         }
-//     });
-//     if(fertilizerToWaterLocations.length === 0) {
-//         fertilizerToWaterLocations = [...soilToFertilizerLocations];
-//     }
-//     console.log('fertilizerToWaterLocations:', fertilizerToWaterLocations);
-//     //
-//     // let waterToLightLocations = [];
-//     // waterToLightMaps.forEach((map) => {
-//     //     if(map.sourceRanges.includes(fertilizerToWaterLocations[fertilizerToWaterLocations.length - 1])) {
-//     //         waterToLightLocations.push(map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         // console.log('map.destinationRanges[map.sourceRanges.indexOf(seed)]', map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         seed = map.destinationRanges[map.sourceRanges.indexOf(seed)];
-//     //     }else {
-//     //         waterToLightLocations.push(seed);
-//     //     }
-//     // });
-//     // console.log('waterToLightLocations:', waterToLightLocations);
-//     //
-//     // let lightToTemperatureLocations = [];
-//     // lightToTemperatureMaps.forEach((map) => {
-//     //     if(map.sourceRanges.includes(waterToLightLocations[waterToLightLocations.length - 1])) {
-//     //         lightToTemperatureLocations.push(map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         // console.log('map.destinationRanges[map.sourceRanges.indexOf(seed)]', map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         seed = map.destinationRanges[map.sourceRanges.indexOf(seed)];
-//     //     }else {
-//     //         lightToTemperatureLocations.push(seed);
-//     //     }
-//     // });
-//     // console.log('lightToTemperatureLocations:', lightToTemperatureLocations);
-//     //
-//     // let temperatureToHumidityLocations = [];
-//     // temperatureToHumidityMaps.forEach((map) => {
-//     //     if(map.sourceRanges.includes(lightToTemperatureLocations[lightToTemperatureLocations.length - 1])) {
-//     //         temperatureToHumidityLocations.push(map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         // console.log('map.destinationRanges[map.sourceRanges.indexOf(seed)]', map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         seed = map.destinationRanges[map.sourceRanges.indexOf(seed)];
-//     //     }else {
-//     //         temperatureToHumidityLocations.push(seed);
-//     //     }
-//     // });
-//     // console.log('temperatureToHumidityLocations:', temperatureToHumidityLocations);
-//     //
-//     // let humidityToLocationLocations = [];
-//     // humidityToLocationMaps.forEach((map) => {
-//     //     if(map.sourceRanges.includes(temperatureToHumidityLocations[temperatureToHumidityLocations.length - 1])) {
-//     //         humidityToLocationLocations.push(map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         // console.log('map.destinationRanges[map.sourceRanges.indexOf(seed)]', map.destinationRanges[map.sourceRanges.indexOf(seed)]);
-//     //         seed = map.destinationRanges[map.sourceRanges.indexOf(seed)];
-//     //     }else {
-//     //         humidityToLocationLocations.push(seed);
-//     //     }
-//     // });
-//     // console.log('humidityToLocationLocations:', humidityToLocationLocations);
-//
-//     console.log('-------------------------');
-// });
+const answer = Math.min(...locations);
+console.log('answer:', answer);
