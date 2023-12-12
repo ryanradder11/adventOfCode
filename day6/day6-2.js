d56ede021837c0a9f1e23c2c365a67f2d2b7d2fc
ryanradder <ryan.radder@headfirst.nl>
2023-12-12 17:44:14 +0100

    -minimize

--- day6/day6-2.js
+++ day6/day6-2.js
@@ -1,44 +1,10 @@
-//time
-let times =   [50748685];
-let records = [242101716911252];
-let possibleWinss = [[]];
-function calculateMaxDistance(index) {
-    let time = times[index];
-    let record= records[index];
-    let maxDistance = 0;
-    for(let i = 0; i < time; i++) {
-        let speed = i;
-        let travelTime = time - i;
-        let distance = speed * travelTime;
-        if(distance > record) {
-            if (!possibleWinss[index]) {
-                possibleWinss[index] = [];
-            }
-            possibleWinss[index].push(distance);
-        }
-        if(distance > maxDistance) {
-            maxDistance = distance;
+getWaysToWin = (raceTime, bestDist ) => {
+    let waysToWin = 0;
+    for( let n = 0; n <= raceTime; n++ ){
+        if( ( ( raceTime - n ) * n ) > bestDist ){
+            waysToWin++;
         }
     }
-
-     getWaysTowin = ( raceTime, bestDist ) => {
-        var waysToWin = 0;
-        for( let n = 0; n <= raceTime; n++ ){
-            // If you hold button n ms, the boat gets a speed of n (mm/ms), for remaining ( length - n ) ms of the race
-            if( ( ( raceTime - n ) * n ) > bestDist ){
-                waysToWin++;
-            }
-        }
-        return waysToWin;
-    }
-    console.log('time: ' + time);
-    console.log('------------------');
-    console.log('maxDistance: ' + maxDistance);
-    console.log('------------------');
-    return maxDistance;
+    return waysToWin;
 }
-let maxDistances = times.map((time, index) => calculateMaxDistance(index));
-console.log(maxDistances);
-console.log(possibleWinss.length);
-console.log('------------------');
-console.log('answser: ',getWaysTowin(times[0], records[0]));
+console.log('Answer: ',getWaysToWin(50748685, 242101716911252));
--- day6/day6.js
+++ day6/day6.js
@@ -1,33 +1,10 @@
-//time
-let times =   [50 ,74, 86, 85];
+let times = [50 ,74, 86, 85];
 let records = [242, 1017, 1691, 1252];
-let possibleWinss = [[]];
-function calculateMaxDistance(index) {
-    let time = times[index];
-    let record= records[index];
-    let maxDistance = 0;
-    for(let i = 0; i < time; i++) {
-        let speed = i;
-        let travelTime = time - i;
-        let distance = speed * travelTime;
-        if(distance > record) {
-            if (!possibleWinss[index]) {
-                possibleWinss[index] = [];
-            }
-            possibleWinss[index].push(distance);
-        }
-        if(distance > maxDistance) {
-            maxDistance = distance;
-        }
-    }
-    console.log('time: ' + time);
-    console.log('------------------');
-    console.log('maxDistance: ' + maxDistance);
-    console.log('------------------');
-    return maxDistance;
+getWaysToWin = (raceTime, bestDist) =>
+    Array.from({length: raceTime + 1}, (_, n) => (raceTime - n) * n > bestDist ? 1 : 0)
+        .reduce((a, b) => a + b, 0);
+let totalScore = 1;
+for( let i = 0, j = times.length; i < j; i++ ){
+    totalScore *= getWaysToWin( times[ i ], records[ i ] );
 }
-let maxDistances = times.map((time, index) => calculateMaxDistance(index));
-console.log(maxDistances);
-console.log(possibleWinss);
-console.log('------------------');
-console.log('anser: ',possibleWinss[0].length * possibleWinss[1].length * possibleWinss[2].length);
+console.log( 'Answer 1, Answer 2: ',totalScore + ' ' + getWaysToWin(parseInt(times.join('')), parseInt(records.join(''))) );
