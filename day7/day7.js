4462f7df9b135934f7361ff61ac1e82e6d0680e2
ryanradder <ryan.radder@headfirst.nl>
2023-12-13 21:34:40 +0100

    Implement poker hand identification in day7.js
    Added several identifiers for distinguishing between different poker hands such as 'five of a kind', 'four of a kind', 'full house' and more in the day7.js script. These allow for more specific outputs when looping through hands, providing a clearer insight into the hand's classification.

--- day7/day7.js
+++ day7/day7.js
@@ -12,6 +12,41 @@ lines.forEach((line) => {
     bets.push(line[1]);
 });
 
+let fiveOfAKind = [7, identifier = (hand) => /^(\w)\1*$/.test(hand)];
+let fourOfAKind = [6, identifier = (hand) => /^(\w)\1{3}$/.test(hand)];
+let fullHouse = [5, identifier = (hand) => /^(\w)\1{2}(\w)\2$/.test(hand)];
+let threeOfAKind = [4, identifier = (hand) => /^(\w)\1{2}$/.test(hand)];
+let twoPair = [3, identifier = (hand) => /^(\w)\1(\w)\2$/.test(hand)];
+let onePair = [2, identifier = (hand) => /^(\w)\1$/.test(hand)];
+let highCard = [1, identifier = (hand) => !/(.).*\1/.test(hand)];
+
 hands.forEach((hand, index) => {
-    console.log(hand, bets[index]);
+    if(fiveOfAKind[1](hand)) {
+        console.log('five of a kind: '+ hand);
+        return;
+    }
+    if(fourOfAKind[1](hand)) {
+        console.log('four of a kind: '+ hand);
+        return;
+    }
+    if(fullHouse[1](hand)) {
+        console.log('full house: '+ hand);
+        return;
+    }
+    if(threeOfAKind[1](hand)) {
+        console.log('three of a kind: '+ hand);
+        return;
+    }
+    if(twoPair[1](hand)) {
+        console.log('two pair: '+ hand);
+        return;
+    }
+    if(onePair[1](hand)) {
+        console.log('one pair: '+ hand);
+        return;
+    }
+    if(highCard[1](hand)) {
+        console.log('high card: '+ hand);
+
+    }
 });
