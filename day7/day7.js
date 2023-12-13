070594a9313f21a5ed1862cd1ba3dc009f8ac157
ryanradder <ryan.radder@headfirst.nl>
2023-12-13 21:41:29 +0100

    Refactor poker hand identification process
    Updated the regular expressions to correctly identify poker hands. The if statements have been refactored into else if statements to optimize execution flow and avoid unnecessary checks. Also, a fallback has been added to handle cases where no specific hand is identified.

--- day7/day7.js
+++ day7/day7.js
@@ -16,8 +16,8 @@ let fiveOfAKind = [7, identifier = (hand) => /^(\w)\1*$/.test(hand)];
 let fourOfAKind = [6, identifier = (hand) => /^(\w)\1{3}$/.test(hand)];
 let fullHouse = [5, identifier = (hand) => /^(\w)\1{2}(\w)\2$/.test(hand)];
 let threeOfAKind = [4, identifier = (hand) => /^(\w)\1{2}$/.test(hand)];
-let twoPair = [3, identifier = (hand) => /^(\w)\1(\w)\2$/.test(hand)];
-let onePair = [2, identifier = (hand) => /^(\w)\1$/.test(hand)];
+let twoPair = [3, identifier = (hand) => /(\w).*\1.*(\w).*\2/.test(hand)];
+let onePair = [2, identifier = (hand) =>  /(.)\1/.test(hand)];
 let highCard = [1, identifier = (hand) => !/(.).*\1/.test(hand)];
 
 hands.forEach((hand, index) => {
@@ -25,28 +25,31 @@ hands.forEach((hand, index) => {
         console.log('five of a kind: '+ hand);
         return;
     }
-    if(fourOfAKind[1](hand)) {
+    else if(fourOfAKind[1](hand)) {
         console.log('four of a kind: '+ hand);
         return;
     }
-    if(fullHouse[1](hand)) {
+    else if(fullHouse[1](hand)) {
         console.log('full house: '+ hand);
         return;
     }
-    if(threeOfAKind[1](hand)) {
+    else if(threeOfAKind[1](hand)) {
         console.log('three of a kind: '+ hand);
         return;
     }
-    if(twoPair[1](hand)) {
+    else if(twoPair[1](hand)) {
         console.log('two pair: '+ hand);
         return;
     }
-    if(onePair[1](hand)) {
+    else if(onePair[1](hand)) {
         console.log('one pair: '+ hand);
         return;
     }
-    if(highCard[1](hand)) {
+    else if(highCard[1](hand)) {
         console.log('high card: '+ hand);
 
+    } else {
+        console.log('no hand: '+ hand);
     }
+
 });
